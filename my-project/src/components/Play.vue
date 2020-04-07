47710dd4f9e68990291a8dbb65d39d3b5762d899
chenmenglong24 <2268234936@qq.com>
2020-04-08 00:05:02 +0800

    播放页面播放音乐

--- my-project/src/components/Play.vue
+++ my-project/src/components/Play.vue
@@ -3,12 +3,16 @@
     <div class="play-box">
       <span @click="back"><mt-cell class="back" icon="back"></mt-cell></span>
       {{msg}}
-      <audio id="audio" 
-        :src="playingSong.url" 
-        >
-      </audio>
       <div class="play-bar">
-      播放页面的footbar
+        <div class="audio-box">
+          <div class="switch-box">
+            <img ref="playBtn" :class="[play ? 'switchPlay' : 'switchPause']" id="switch" src="../assets/play.png" @click="playPause"/>
+          </div>
+          <audio id="audio" 
+            :src="playingSong.url" 
+            >
+          </audio>
+        </div>
       </div>
     </div>
   <!-- </transition> -->
@@ -20,19 +24,18 @@ export default {
     return {
       msg: '我是播放页面',
       see: true,
+      play: true,
       playingSong: {}
     }
   },
   created() {
     this.playingSong = this.$store.state.playingSong
-    var audio = document.getElementById('audio')
-    // if (audio.paused) {
-        audio.play();
-        // btn.src = "/static/play.png";
-      // } else {
-        // audio.pause();
-        // btn.src = "/static/pause.png";
-      // }
+    // console.log(this.playingSong.url)
+  },
+  mounted() {
+    let audio = document.getElementById('audio')
+    audio.play()
+    console.log('页面加载进来时audio.paused：',audio.paused)
   },
   methods: {
     back () {
@@ -40,19 +43,37 @@ export default {
     },
     show () {
       this.see = !this.see
+    },
+    playPause () {
+      var audio = document.getElementById('audio');
+      var playBtn = this.$refs.playBtn;
+      if (audio.paused) {
+        audio.play();
+        console.log('点击播放按钮时audio.paused：',audio.paused)
+        this.play = true
+        playBtn.src = "/static/play.png";
+      } else {
+        audio.pause();
+        console.log('点击暂停按钮时audio.paused：',audio.paused)
+        this.play = false
+        playBtn.src = "/static/pause.png"
+        // let switchNode = document.getElementById('switch')
+        // console.log(switchNode)
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-/* .play-box{
-  
-} */
+.play-box{
+  background-color: #eeeeee;
+  height: 100vh;
+}
 .play-bar{
   width: 100%;
-  height: 50px;
-  background-color: #999999;
+  height: 120px;
+  background-color: #ffffff;
   bottom: 0;
   left: 0;
   position: fixed;
@@ -70,4 +91,27 @@ export default {
 /* .play-page-leave, .play-page-enter-active{
   transform: translateY(500px)
 } */
+.switch-box{
+  width: 80px;
+  height: 80px;
+  background-color: #eeeeee;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 20px);
+  border-radius: 50%;
+}
+.switchPlay{
+  width: 50px;
+  height: 50px;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 15px);
+}
+.switchPause{
+  width: 50px;
+  height: 50px;
+  position: relative;
+  left: 50%;
+  transform: translate(-43%, 15px);
+}
 </style>
